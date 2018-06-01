87661702e7fd1840cbc0ec37cc85f5c8e0739867
Kevin Kim <kimkevin99@outlook.com>
2018-06-01 09:52:49 -0700

    formatting
    just formatting changes

--- src/groupSteganography/Comparator.java
+++ src/groupSteganography/Comparator.java
@@ -2,21 +2,21 @@ package groupSteganography;
 
 public class Comparator { // to compare and eventually decode
 	private int[][] original;
-	private int[][] after; 
-	private String toReturn; 
-	
-	
-	public Comparator(int[][] original, int[][] after) { //assume that the images are the same size
+	private int[][] after;
+	private String toReturn;
+
+	public Comparator(int[][] original, int[][] after) { // assume that the images are the same size
 		this.original = original;
-		this.after = after; 
+		this.after = after;
 		compare();
 	}
-	private void compare() { //assumes that the message was encoded in order
+
+	private void compare() { // assumes that the message was encoded in order
 		StringBuilder sb = new StringBuilder();
-		char a; 
-		for(int i = 0; i<original.length; i++) {
-			for(int j = 0; j<original[i].length; j++) {
-				if(original[i][j] != after[i][j]) {
+		char a;
+		for (int i = 0; i < original.length; i++) {
+			for (int j = 0; j < original[i].length; j++) {
+				if (original[i][j] != after[i][j]) {
 					a = (char) after[i][j];
 					sb.append(Character.toString(a));
 				}
@@ -24,11 +24,9 @@ public class Comparator { // to compare and eventually decode
 		}
 		toReturn = sb.toString();
 	}
-	
+
 	public String returnMessage() {
-		return toReturn; 
+		return toReturn;
 	}
-	
 
 }
- 
