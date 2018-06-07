800763a90468dda7a7089246448e9b46fad161cf
dhw0 <39346736+dhw0@users.noreply.github.com>
2018-06-07 09:27:52 -0700

    Update Main.java

--- src/groupSteganography/Main.java
+++ src/groupSteganography/Main.java
@@ -34,31 +34,5 @@ public class Main {
 		}
 	}
 
-	public static void imageToBinary() throws IOException {
-		BufferedImage image = ImageIO.read(new File("src/groupSteganography/csimage.jpg"));
-		int r = 0, g = 0, b = 0;
-		int w = image.getWidth();
-		int h = image.getHeight();
-		System.out.println("Image Dimension: Height-" + h + ", Width-" + w);
-		int totalPixels = (h * w);
-		ArrayList<Color> Colors = new ArrayList<Color>();
-		for (int i = 0; i < w; i++) {
-			for (int j = 0; j < h; j++) {
-				Colors.add(new Color(image.getRGB(i, j)));
-			}
-		}
-		for (int i = 0; i < Colors.size(); i++) {
-			Color c = Colors.get(i);
-			r = c.getRed();
-			g = c.getGreen();
-			b = c.getBlue();
-			/*
-			System.out.println("Element: " + (i + 1));
-			System.out.println("Red: " + Integer.toString(r, 2)); // converts to binary
-			System.out.println("Green: " + Integer.toString(g, 2));
-			System.out.println("Blue: " + Integer.toString(b, 2));
-			System.out.println("");
-			*/
-		}
-	}
+	
 }
