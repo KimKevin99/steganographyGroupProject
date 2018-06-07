506534c79f04fe37c49d43c39aaa9570eaad7eb2
Kevin Kim <kimkevin99@outlook.com>
2018-06-07 09:39:52 -0700

    added package
    formatting

--- src/groupSteganography/FastRGB.java
+++ src/groupSteganography/FastRGB.java
@@ -1,44 +1,40 @@
+package groupSteganography;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
-public class FastRGB
-{
+public class FastRGB {
 
-    private int width;
-    private int height;
-    private boolean hasAlphaChannel;
-    private int pixelLength;
-    private byte[] pixels;
+	private int width;
+	private int height;
+	private boolean hasAlphaChannel;
+	private int pixelLength;
+	private byte[] pixels;
 
-    FastRGB(BufferedImage image)
-    {
+	FastRGB(BufferedImage image) {
 
-        pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        width = image.getWidth();
-        height = image.getHeight();
-        hasAlphaChannel = image.getAlphaRaster() != null;
-        pixelLength = 3;
-        if (hasAlphaChannel)
-        {
-            pixelLength = 4;
-        }
+		pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		width = image.getWidth();
+		height = image.getHeight();
+		hasAlphaChannel = image.getAlphaRaster() != null;
+		pixelLength = 3;
+		if (hasAlphaChannel) {
+			pixelLength = 4;
+		}
 
-    }
+	}
 
-    int getRGB(int x, int y)
-    {
-        int pos = (y * pixelLength * width) + (x * pixelLength);
+	int getRGB(int x, int y) {
+		int pos = (y * pixelLength * width) + (x * pixelLength);
 
-        int argb = -16777216; // 255 alpha
-        if (hasAlphaChannel)
-        {
-            argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
-        }
+		int argb = -16777216; // 255 alpha
+		if (hasAlphaChannel) {
+			argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
+		}
 
-        argb += ((int) pixels[pos++] & 0xff); // blue
-        argb += (((int) pixels[pos++] & 0xff) << 8); // green
-        argb += (((int) pixels[pos++] & 0xff) << 16); // red
-        return argb;
-    }
+		argb += ((int) pixels[pos++] & 0xff); // blue
+		argb += (((int) pixels[pos++] & 0xff) << 8); // green
+		argb += (((int) pixels[pos++] & 0xff) << 16); // red
+		return argb;
+	}
 }
